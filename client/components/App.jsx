767d3856d373626cce4d4f3ddd6d976075f524a0
ariana-mirus <arianamirus@gmail.com>
2020-07-04 07:51:08 +1200

    Revert "deleted unused files from boilerplate set up"
    This reverts commit 46ef803a36fa8f96676f35f58580c449c4f29083.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import Words from './Words'
+import AddWord from './AddWord'
+
 const App = () => (
   <div className='app-container'>
-
+    <Words />
+    <AddWord />
   </div>
 )
 
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,5 +1,7 @@
-import { combineReducers } from 'redux'
+import {combineReducers} from 'redux'
+
+import words from './words'
 
 export default combineReducers({
-
+  words
 })
